1b222799435db0ff25a9a203dd6bed28efbea420
Kazan <andyswebking@gmail.com>
2023-07-14 11:15:48 +0800

    Init project and add bot main program

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+import schedule
+import telebot
+
+from datetime import date, datetime, time
+from pytz import timezone
+
+API_TOKEN = '6062089972:AAE2HvFkNJlSNualjVSXE472SLDp8hnGlZI'
+
+bot = telebot.TeleBot(API_TOKEN)
+chat_id = -1001967255559
+timearea = timezone('Asia/Taipei')
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+# Time getting
+@bot.message_handler(commands=['time'])
+def send_time():
+    time_now = date.today().strftime("%Y-%m-%d")
+    bot.send_message(chat_id=chat_id, text=time_now)
+    
+
+bot.infinity_polling()
