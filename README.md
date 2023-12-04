da703501cb24b8759d7ac7164a366bedb8514c1a
Kazan <andyswebking@gmail.com>
2023-12-04 11:08:22 +0800

    Delete TOKEN and chat ID for open source

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # camp2023-story-bot
-Auto-sending bot for story in SITCON Camp 2023
+Telegram story bot written in Python for SITCON Camp 2023
+
+## 敘述
+- SITCON Camp 2023 故事半自動推送機器人，使用本屆教學 `pyTelegramBotAPI` 以及 `poetry` 虛擬環境。
+
+## Dependency
+- Python >= 3.11
+- poetry >= 1.5.1
+- pyTelegramBotAPI >= 4.12.0
+
+## Setup
+### 安裝 poetry
+- 安裝
+```bash
+$ curl -sSL https://install.python-poetry.org | python3 -
+```
+
+- 設定環境變數
+```bash
+$ echo 'export PATH="~/.local/bin:$PATH"' >> ~/.bashrc
+```
+
+- 重啟終端並測試
+```bash
+$ poetry --version
+```
+
+### 安裝專案套件
+```bash
+$ poetry install
+```
+或者可以自行初始化（請先刪除 `pyproject.toml`）
+```bash
+$ poetry init
+```
+設定虛擬環境，並透過 `poetry add` 增加所需套件。
+
+### 開發與執行
+- 需要啟動 poetry shell，方可編輯程式碼與執行：
+```bash
+$ poetry shell
+```
+
+## 使用方式
+1. 將故事文檔轉為 JSON 格式，並參考 `story/` 內範例，拆分各章節為單一檔案（幾個章節至少要有幾個檔案）；需要注意，每個檔案最大不能超過 20 則訊息（20 個 key），若超過擇需要再拆分。
+2. 若是傳送圖片，請將圖片（PNG 檔）放置在 `img/`，並將 `"note"` 的值改為 `yes`，`"story"` 填上圖片檔名；圖片限制請參考 [API 說明](https://core.telegram.org/bots/api#sendphoto)。
+3. 將 `main.py` 內故事推送的 `commands` 填入 `story/` 中各個檔案的檔名（如第一章為 `ch1`……），有拆分的章節全部都要填入。
+4. 依據流程新增 bot 及頻道後，將 bot token 及頻道的 `chat_id` 填入。
+5. 傳送時，在 poetry shell 下執行 `main.py`，並在 bot 的聊天室輸入 `/{章節名稱}`，即可傳送。
+
+## 注意事項
+- 故事內文可以使用 Markdown V2 語法，詳細請參考[API 說明](https://core.telegram.org/bots/api#markdownv2-style)。
+- 圖片傳送使用 `InputFile`（[說明](https://core.telegram.org/bots/api#inputfile)）。
+- 如果在自己的機器上直接執行程式，可能會發生 time out，所以傳送完畢建議就中止執行。
--- main.py
+++ main.py
@@ -1,29 +1,70 @@
-import requests
-import schedule
+import json
 import telebot
+import time
 
-from datetime import date, datetime, time
-from pytz import timezone
+from telebot.types import InputFile
 
 API_TOKEN = 'TOKEN'
 
 bot = telebot.TeleBot(API_TOKEN)
-chat_id = -1001967255559
-timearea = timezone('Asia/Taipei')
+# chat_id = ID   # Test Channel
+chat_id = 'ID'  # Open Channel
 
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+# Handle story
 
-# Time getting
-@bot.message_handler(commands=['time'])
-def send_time():
-    time_now = date.today().strftime("%Y-%m-%d")
-    bot.send_message(chat_id=chat_id, text=time_now)
-    
+
+@bot.message_handler(commands=['ch0',
+                               'ch1',
+                               'ch2',
+                               'ch3',
+                               'ch3_5',
+                               'ch4',
+                               'ch5',
+                               'ch6',
+                               'ch7',
+                               'ch8',
+                               'ch9',
+                               'ch10',
+                               'ch11',
+                               'ch11_5',
+                               'ch12',
+                               'ch13',
+                               'ch14',
+                               'ch15',
+                               'ch15_5',
+                               'ch15_6',
+                               'ch16',
+                               'ch17',
+                               'ch18',
+                               'ch19',
+                               'ch19_2',
+                               'ch19_5',
+                               'ch20',
+                               'ch21',
+                               'ch21_2',
+                               'ch21_3',
+                               'ch21_4',
+                               'ch22',
+                               'ch22_2',
+                               'ch22_3',
+                               'bonus',
+                               'final'])
+def send_story(message):
+    chapter = message.text.split()[0][1:]
+    file = "./story/" + chapter + ".json"
+    with open(file) as f:
+        data = json.load(f)
+
+    for i in data:
+        if i['note'] == 'yes':
+            photo_path = './img/' + i['story'] + '.png'
+            bot.send_photo(chat_id=chat_id, photo=InputFile(photo_path))
+        else:
+            bot.send_message(
+                chat_id=chat_id,
+                text=i['story'],
+                parse_mode="MarkdownV2")
+        time.sleep(1)
+
 
 bot.infinity_polling()
